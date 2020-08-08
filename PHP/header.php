72f2b4ce116fa64079c199ff160dc44ecce796f6
cmcin019 <25cristopher25@gmail.com>
2020-08-08 11:51:04 -0400

    Header nav and CSS

--- PHP/header.php
+++ PHP/header.php
@@ -1,15 +1,25 @@
 <div class="header">
-    <div class="header-menu">
-        <ul>
-            <li>
-                <span>Join group</span>
-            </li>
-            <li>
-                <span>Find Friend</span>
-            </li>
-            <li>
-                <span>Settings</span>
-            </li>
-        </ul>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="header-logo col-6">
+                <i class="far fa-comments"></i><span> Coll3ctio</span>
+            </div>
+            <div class="header-menu co-6">
+                <ul>
+                    <li>
+                        <span>Join group</span>
+                    </li>
+                    <li>
+                        <span>Find Friend</span>
+                    </li>
+                    <li>
+                        <span>Settings</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
--- index.php
+++ index.php
@@ -2,11 +2,18 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <title></title>
+        <!-- CSS -->
         <link rel="stylesheet" type="text/css" href="Styles/global.css">
         <link rel="stylesheet" type="text/css" href="Styles/header.css">
-
+        
         <link rel="stylesheet" type="text/css" href="Styles/index.css">
-        <title></title>
+
+        <!-- BOOTSTRAP -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+        <!-- FONT AWESOME -->
+        <script src="https://kit.fontawesome.com/f45e40cf2b.js" crossorigin="anonymous"></script>
 
     </head>
     <body>
