4fcbdca2ff877121362c0acac80f41ef71bda46c
cmcin019 <25cristopher25@gmail.com>
2020-08-08 10:04:52 -0400

    Add Header

---
+++ Includes/dbh.inc.php
@@ -0,0 +1,10 @@
+<?php
+	$dbServername = "localhost";
+	$dbUsername = "root";
+	$dbPassword = "";
+	$dbName = "db_hack";
+
+	$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+	session_start();
+?>
--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="Styles/global.css">
+        <link rel="stylesheet" type="text/css" href="Styles/index.css">
         <title></title>
 
     </head>
     <body>
-        HelloWorld
+        <div>
+            <h1>Welcom to Col3ctio</h1>
+        </div>
     </body>
 
 
