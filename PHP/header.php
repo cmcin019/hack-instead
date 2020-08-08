784d1a531a44d73f961f3cc3fe8aa50ebadc8cb9
cmcin019 <25cristopher25@gmail.com>
2020-08-08 10:44:24 -0400

    Update Header

--- PHP/header.php
+++ PHP/header.php
@@ -1,13 +1,15 @@
-<div>
-
-    <ul>
-        <li>
-            
-        </li>
-        <li>
-        </li>
-        <li>
-        </li>
-    </ul>
-
+<div class="header">
+    <div class="header-menu">
+        <ul>
+            <li>
+                <span>Join group</span>
+            </li>
+            <li>
+                <span>Find Friend</span>
+            </li>
+            <li>
+                <span>Settings</span>
+            </li>
+        </ul>
+    </div>
 </div>
--- index.php
+++ index.php
@@ -3,13 +3,16 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="Styles/global.css">
+        <link rel="stylesheet" type="text/css" href="Styles/header.css">
+
         <link rel="stylesheet" type="text/css" href="Styles/index.css">
         <title></title>
 
     </head>
     <body>
+        <?php include_once 'PHP/header.php' ?>
         <div>
-            <h1>Welcom to Col3ctio</h1>
+            <h1>Welcom to Coll3ctio</h1>
         </div>
     </body>
 
